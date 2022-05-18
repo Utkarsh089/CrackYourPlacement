72f4b8bff34d011111abbc9e508b2be7a075ecb1
Utkarsh Paliwal <64469470+Utkarsh089@users.noreply.github.com>
2022-05-18 23:59:15 +0530

    Day 6

---
+++ Linked List/Rearrange a given linked list in-place.cpp
@@ -0,0 +1,112 @@
+// https://www.geeksforgeeks.org/rearrange-a-given-linked-list-in-place/
+
+// { Driver Code Starts
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+/* Linked list Node */
+struct Node {
+    int data;
+    struct Node* next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+void reorderList(struct Node* head);
+
+/* Function to create a new Node with given data */
+struct Node* newNode(int data) {
+    struct Node* new_Node = new Node(data);
+    new_Node->data = data;
+    new_Node->next = NULL;
+    return new_Node;
+}
+
+void printList(struct Node* Node) {
+    while (Node != NULL) {
+        printf("%d ", Node->data);
+        Node = Node->next;
+    }
+    printf("\n");
+}
+
+void freeList(struct Node* head) {
+    struct Node* temp;
+    while (head != NULL) {
+
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+}
+
+int main(void) {
+    int t, n, m, i, x;
+
+    cin >> t;
+    while (t--) {
+        struct Node* temp, *head;
+        cin >> n;
+        cin >> x;
+        head = new Node(x);
+        temp = head;
+        for (i = 0; i < n - 1; i++) {
+            cin >> x;
+            temp->next = new Node(x);
+            temp = temp->next;
+        }
+
+        reorderList(head);
+        printList(head);
+        freeList(head);
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+/* Following is the Linked list node structure */
+
+/*
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+Node* cur = NULL;
+void recursiveHelper(Node* head){
+    if(head==NULL)
+        return;
+    recursiveHelper(head->next);
+    if(cur==head){  //case1
+        cur->next = NULL;
+        return;
+    }
+    if(cur->next){
+        Node* next = cur->next;
+        cur->next = head;
+        cur = cur->next;
+        cur->next = next;
+        cur = cur->next;
+         if(cur->next==head){   //case2
+            cur->next = NULL;
+        }
+    }
+}
+
+void reorderList(Node* head) {
+    // Your code here
+    cur = head;
+    recursiveHelper(head);
+}
+
