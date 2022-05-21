2f21c007519c00d5e9f40521077741d2e0dbbfba
Utkarsh Paliwal <64469470+Utkarsh089@users.noreply.github.com>
2022-05-21 21:03:21 +0530

    Tree

---
+++ Day Wise/Day 9/43 Maximum Depth of Binary Tree.cpp
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/maximum-depth-of-binary-tree/
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root==NULL)
+            return 0;
+        return max(maxDepth(root->left), maxDepth(root->right)) + 1;
+    }
+};
